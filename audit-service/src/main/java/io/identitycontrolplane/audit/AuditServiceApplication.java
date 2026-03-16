90b94bb505923e7bffbdae6364387592bd80cf30
shsssnk26 <shashsecprep@gmail.com>
2026-03-15 19:57:58 -0500

    Adding initial application.java files

---
+++ audit-service/src/main/java/io/identitycontrolplane/audit/AuditServiceApplication.java
@@ -0,0 +1,11 @@
+package io.identitycontrolplane.audit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuditServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AuditServiceApplication.class, args);
+    }
+}
